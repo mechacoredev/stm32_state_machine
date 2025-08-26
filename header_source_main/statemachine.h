8f4dd833b5a418f935bdbfebe6b85935557d526c
mechacoredev <eneserbulut67890@gmail.com>
2025-08-26 13:11:01 +0300

    Create statemachine.h

---
+++ header_source_main/statemachine.h
@@ -0,0 +1,37 @@
+/*
+ * statemachine.h
+ *
+ *  Created on: Aug 26, 2025
+ *      Author: Enes
+ */
+
+#ifndef INC_STATEMACHINE_H_
+#define INC_STATEMACHINE_H_
+
+#include "stm32f4xx_hal.h"
+#include <stdbool.h>
+
+struct state_machine;
+typedef struct state_machine* state_machine_handle;
+
+typedef enum{
+	state_1,
+	state_2,
+	state_3,
+	state_4,
+	state_5
+}state_type;
+
+typedef struct{
+	GPIO_TypeDef* leds_port;
+	uint16_t led1_pin;
+	uint16_t led2_pin;
+	uint16_t led3_pin;
+	uint16_t led4_pin;
+}state_machine_configs;
+
+state_machine_handle state_machine_init(state_machine_configs* config);
+
+void StateMachine_Run(state_machine_handle dev);
+
+#endif /* INC_STATEMACHINE_H_ */
